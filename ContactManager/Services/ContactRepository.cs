a76749823aac9ef4a0589475b9791872a7c8ab60
roko-barbic <rokobarosk12345@gmail.com>
2023-01-23 20:52:22 +0100

    ContactManager

--- ContactManager/Services/ContactRepository.cs
+++ ContactManager/Services/ContactRepository.cs
@@ -3,26 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ContactManager.Models;
+using Microsoft.AspNetCore.Http;
+using System.Web;
+using Newtonsoft.Json;
 
 namespace ContactManager.Services
 {
     public class ContactRepository
     {
-        public Contact[] GetAllContacts()
+        private const string CacheKey = "ContactStore";
+        public ContactRepository(IHttpContextAccessor httpContextAccessor)
         {
-            return new Contact[]
+            var ctx = httpContextAccessor.HttpContext;
+            if (ctx != null)
             {
-            new Contact
-            {
-                Id = 1,
-                Name = "Glenn Block"
-            },
-            new Contact
-            {
-                Id = 2,
-                Name = "Dan Roth"
+                var contacts = new Contact[]
+                {
+            new Contact { Id = 1, Name = "Glenn Block" },
+            new Contact { Id = 2, Name = "Dan Roth" }
+                };
+                ctx.Session.SetString(CacheKey, JsonConvert.SerializeObject(contacts));
             }
-            };
         }
+
+        public Contact[] GetAllContacts(IHttpContextAccessor httpContextAccessor)
+        {
+            var ctx = httpContextAccessor.HttpContext;
+            if (ctx != null)
+            {
+                var contactsJson = ctx.Session.GetString(CacheKey);
+                return JsonConvert.DeserializeObject<Contact[]>(contactsJson);
+            }
+            return new Contact[] { };
+        }
+
     }
 }
